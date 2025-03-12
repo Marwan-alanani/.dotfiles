1bba6766efd2c772aabdbe6de8a1915c025e9c1d
Marwan <marwan-walid1@hotmail.com>
2025-03-12 08:37:48 +0200

    Added Some extra remaps

--- nvim/lua/custom/plugins/colors.lua
+++ nvim/lua/custom/plugins/colors.lua
@@ -14,7 +14,25 @@ return {
     name = 'rose-pine',
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('rose-pine').setup {}
+      require('rose-pine').setup {
+        styles = {
+          bold = false,
+          italic = false,
+        },
+        palette = {
+          moon = {
+            surface = '#333333', -- window colors
+            base = '#272727', -- this is the background color
+            overlay = '#333333', -- highlight color
+          },
+          main = {
+            surface = '#333333',
+            base = '#222222',
+            overlay = '#333333',
+          },
+        },
+      }
+      vim.cmd 'colorscheme rose-pine-moon'
     end,
   },
 
@@ -31,7 +49,6 @@ return {
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('nordic').setup {}
-      vim.cmd 'colorscheme nordic'
     end,
   },
   {
@@ -48,6 +65,17 @@ return {
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('nightfox').setup()
+      --vim.cmd 'colorscheme nordfox'
+    end,
+  },
+
+  {
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    config = function()
+      require('github-theme').setup {
+        -- ...
+      }
     end,
   },
 }
