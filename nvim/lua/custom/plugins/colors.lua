161356535a1ee7634335fd988a7dbed0243e08fb
Marwan <marwan-walid1@hotmail.com>
2025-03-23 00:26:39 +0200

    random changes

--- nvim/lua/custom/plugins/colors.lua
+++ nvim/lua/custom/plugins/colors.lua
@@ -1,15 +1,5 @@
 return {
   {
-    'https://github.com/rebelot/kanagawa.nvim.git',
-    name = 'kanagawa',
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('kanagawa').setup {
-        theme = 'dragon',
-      }
-    end,
-  },
-  {
     'https://github.com/rose-pine/neovim.git',
     name = 'rose-pine',
     config = function()
@@ -25,8 +15,6 @@ return {
             surface = '#2a2a2a', -- window colors
             base = '#1a1a1a', -- this is the background color
             overlay = '#2a2a2a', -- highlight color
-
-            -- foam = '#ffffff', --variable colors
           },
           main = {
             surface = '#333333',
@@ -46,29 +34,13 @@ return {
       require('tokyonight').setup {}
     end,
   },
+
   {
     'https://github.com/AlexvZyl/nordic.nvim.git',
     name = 'nordic',
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('nordic').setup {}
-    end,
-  },
-  {
-    'https://github.com/ellisonleao/gruvbox.nvim.git',
-    name = 'gruvbox',
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('gruvbox').setup {}
-    end,
-  },
-  {
-    'EdenEast/nightfox.nvim',
-    name = 'nightfox',
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('nightfox').setup()
-      --vim.cmd 'colorscheme nordfox'
     end,
   },
 
@@ -78,6 +50,16 @@ return {
     config = function()
       require('github-theme').setup {
         -- ...
+      }
+    end,
+  },
+
+  {
+    'https://github.com/navarasu/onedark.nvim.git',
+    name = 'onedark',
+    config = function()
+      require('onedark').setup {
+        style = 'warmer',
       }
     end,
   },
